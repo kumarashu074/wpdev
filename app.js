0d3fa1742a9f2b7695831530ca849fc356b33121
Bruno Lopes <bruno.lourenco.lopes@gmail.com>
2017-11-17 15:39:37 +0000

    aa

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const express = require('express');
-const app = express();
-const path = require('path');
+let express = require('express'),
+    path = require('path');
+var app = express();
+let server = require('http').Server(app);
 
+app.use(express.static(path.join(__dirname)));
 
-app.use(express.static(path.join(__dirname,'dist')));
-app.listen(process.env.PORT || 8080);
+app.get('/', function(req, res, next){
+    res.sendStatus(200);
+});
 
-console.log('console listening');
+app.get('/blog.html', function(req, res,next){
+    res.sendFile(path.join(__dirname+"/blog.html"));
+});
 
+app.post('/contact', function(req, res, next){
+
+});
+server.listen(process.env.PORT || 8000, function() {
+    console.log("App is running");
+});
