b2004c9e39a6106251cac3b6a2bd80c35834437c
Bruno Lopes <bruno.lourenco.lopes@gmail.com>
2017-11-17 15:26:29 +0000

    aa

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname,'dist')));
 app.listen(process.env.PORT || 8080);
-app.get('/*', function (req,res) => {
-  res.sendFile(path.join(__dirname,'/dist/index.html'));	
-});
 
 console.log('console listening');
 
