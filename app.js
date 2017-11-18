565fc95c40d8698b48f264e85044ae2fa8570109
Bruno Lopes <bruno.lourenco.lopes@gmail.com>
2017-11-18 10:23:20 +0000

    aa

--- app.js
+++ app.js
@@ -3,16 +3,26 @@ var app = express();
 let server = require('http').Server(app);
 
 
-var path = require('path');
-var rootPath = path.normalize(__dirname + '/');
-app.use(express.static(rootPath + '/dist'));
+app.set('port', (process.env.PORT || 5000));
 
-app.get('*', function (req, res) {
-  const index = path.join(__dirname,'../dist/index.html');
-  res.sendFile(index);
+app.use(express.static(path.join(__dirname, 'dist/')));
+console.log("path******************: ", path.join(__dirname, 'dist/'));
+
+
+app.get('/', function(req, res, next){
+    res.send('index.html11111111');
 });
 
-var webapp = app.listen(process.env.PORT || 8080, function () {
-    var port = webapp.address().port;
-    console.log("App now running on port", port);
-  });
+//app.get('*', function (req, res) {
+//  const index = path.join(__dirname,'../dist/index.html');
+//  res.sendFile(index);
+//});
+
+app.listen(app.get('port'), function(){
+    console.log('Node app is running on port', app.get('port'));
+})
+
+//var webapp = app.listen(process.env.PORT || 8080, function () {
+ //   var port = webapp.address().port;
+ //   console.log("App now running on port", port);
+ // });
